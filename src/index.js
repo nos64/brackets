f7bde58ca8e5e1466e3b149e1b749630b6bf0496
Mikhail Nosov <manosov1984@gmail.com>
2022-02-26 00:00:13 +0400

    feat: 14/20

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  
+  let OPEN_BRACKETS = []; 
+  let BRACKETS_PAIR = {};
+  
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    OPEN_BRACKETS.push(bracketsConfig[i][0]);
+    BRACKETS_PAIR[bracketsConfig[i][1]] = bracketsConfig[i][0]
+  }
+
+  // console.log('OPEN_BRACKETS: ', OPEN_BRACKETS);
+  // console.log('BRACKETS_PAIR: ', BRACKETS_PAIR);
+
+  for (let i = 0; i < str.length; i++) {
+    let currentSymbol = str[i];
+    // console.log('currentSymbol: ', currentSymbol);
+  
+    if (OPEN_BRACKETS.includes(currentSymbol)) {
+
+        stack.push(currentSymbol);
+        // console.log('stack: ', stack);
+
+    }  else {
+      if (stack.length === 0) {
+        return false;
+      }
+
+      let topElement = stack[stack.length - 1];
+      // console.log('topElement: ', topElement);
+      if (BRACKETS_PAIR[currentSymbol] === topElement) {
+        stack.pop();
+      } else {
+        return false
+      }
+    }
+    for (let i = 0; i < stack.length; i++) {
+      if (stack[i] === stack[i-1]) stack = []
+    }
+  } 
+return stack.length === 0;
 }
